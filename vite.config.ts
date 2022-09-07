01929743871cf6fb5487f80c0d2db7092ec3bf8c
brojor <bronislav.jordan@gmail.com>
2022-09-07 11:44:45 +0200

    chore: add auto import APIs on-demand

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Unocss from 'unocss/vite';
 import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,15 @@ export default defineConfig({
     Unocss(),
     Components({
       dts: true,
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+      ],
+      dts: true,
+      vueTemplate: true,
     }),
   ],
   resolve: {
