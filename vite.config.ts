2fa6a567452aac9d090974722eb90a983d7b7a57
brojor <bronislav.jordan@gmail.com>
2022-09-07 11:41:37 +0200

    chore: add on-demand components auto importing

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Unocss from 'unocss/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),  Unocss()],
+  plugins: [
+    vue(),
+    Unocss(),
+    Components({
+      dts: true,
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
