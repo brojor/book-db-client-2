013768f9d3deb20b850de320f0248618bb1cf8a6
brojor <bronislav.jordan@gmail.com>
2022-09-07 08:38:19 +0200

    chore: Add runtime for building Web Native apps

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'me.brojor.books',
+  appName: 'book-db-client-2',
+  webDir: 'dist',
+  bundledWebRuntime: true,
+  backgroundColor: '#ffffff',
+};
+
+export default config;
