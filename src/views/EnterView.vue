5289bb481c763650e29297a26ab2ee2a763f0be3
brojor <bronislav.jordan@gmail.com>
2022-09-10 12:39:41 +0200

    refactor(EnterView): remove unused code

--- src/views/EnterView.vue
+++ src/views/EnterView.vue
@@ -1,59 +1,10 @@
 <script setup lang="ts">
-import apiService from '@/services/api'
-
-const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-
-interface Credentials {
-  email: string
-  password: string
-  rememberMe: boolean
-}
 type FormType = 'register' | 'login'
 
-const user = useUserStore()
-const collectionStore = useCollectionStore()
-const router = useRouter()
-
-const credentials = ref<Credentials>({
-  email: '',
-  password: '',
-  rememberMe: false,
-})
-
-const loginError = ref<boolean>(false)
-
-const formType = ref<FormType>('register')
-
-// const emailIsValid = computed(() => {
-//   return credentials.value.email.length > 0 && emailRegex.test(credentials.value.email)
-// })
-// const passwordIsValid = computed(() => {
-//   return credentials.value.password.length > 8
-// })
+const formType = ref<FormType>('login')
 
 const changeFormType = () => {
   formType.value = formType.value === 'register' ? 'login' : 'register'
-}
-
-const login = async () => {
-  loginError.value = false
-  try {
-    const response = await apiService.post(`/${formType.value}`, credentials)
-    const token = response.data.token
-    user.setToken(token, { rememberMe: credentials.value.rememberMe })
-    await collectionStore.getCollection('default')
-    await collectionStore.getCollection('wishlist')
-    router.push('/')
-  }
-  catch (error: any) {
-    if (error.response.data.error === 'Invalid credentials')
-      loginError.value = true
-  }
-}
-
-const handleSubmit = async () => {
-  // if (emailIsValid.value && passwordIsValid.value)
-  login()
 }
 </script>
 
@@ -65,8 +16,8 @@ const handleSubmit = async () => {
     <p text-center m="y-3 x-auto" w="5/6">
       Na místě, kde máte dokonalý přehled o své sbírce knih.
     </p>
-    <EnterForm v-model="credentials" :form-type="formType" :login-error="loginError" />
-    <p mt8>
+    <EnterForm :form-type="formType" />
+    <p mt12>
       {{ formType === 'register' ? 'Máte již účet?' : 'Nemáte účet? ' }}
       <a text-teal-500 tabIndex="0" @click="changeFormType">{{
         formType === 'register' ? 'Přihlašte se' : 'Zaregistrujte se'
