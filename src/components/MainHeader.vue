71f0c1987f9dabfcffc4dc6c6d84183568c2ad5c
brojor <bronislav.jordan@gmail.com>
2022-09-11 13:44:00 +0200

    feat(MainHeader): Delete text in searchbar on click on cross button

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,33 +1,34 @@
 <script setup lang="ts">
 const user = useUserStore()
 const searchbar = useSearchStore()
-const route = useRoute()
 
-const placeholder = computed(() => {
-  if (route.name === 'authors')
-    return 'Hledat v autorech'
-  else
-    return 'Hledat v knihách'
-})
-
-const handleInput = (e: Event) => {
-  const input = e.target as HTMLInputElement
-  searchbar.setValue(input.value)
-}
-
+const placeholder = computed(() => `Hledat v ${searchbar.display === 'books' ? 'knihách' : 'autorech'}`)
 const handleLogout = () => {
   user.logout()
+}
+const deleteSearchbarValue = (e: Event) => {
+  searchbar.clean()
+  const button = e.target as HTMLButtonElement
+  const input = button.previousElementSibling as HTMLInputElement
+  input.focus()
+}
+
+const buttonHandler = (e: Event) => {
+  if (searchbar.value)
+    deleteSearchbarValue(e)
+  else
+    handleLogout()
 }
 </script>
 
 <template>
   <header flex relative bg-canvas px4 py2>
     <AInput
-      v-model="searchbar.value" :placeholder="placeholder" input-wrapper-classes="rounded-full"
-      prepend-inner-icon="i-material-symbols:search" bg-base
+      v-model="searchbar.value" :placeholder="placeholder"
+      input-wrapper-classes="rounded-full" prepend-inner-icon="i-material-symbols:search" bg-base
     >
       <template #append-inner>
-        <button :class="searchbar.value ? 'i-mdi:close:' : 'i-mdi:dots-vertical'" right-4 absolute />
+        <button :class="searchbar.value ? 'i-mdi:close:' : 'i-mdi:dots-vertical'" right-4 absolute @click="buttonHandler" />
       </template>
     </AInput>
   </header>
