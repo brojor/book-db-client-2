5ef2593f87ae34eef161c9e2e176a3e636df517a
brojor <bronislav.jordan@gmail.com>
2022-09-12 15:18:46 +0200

    feat(listItem): add ability to visual select item

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -4,10 +4,39 @@ defineProps<{
   subtitle: string | number
   icon: 'author' | 'book'
 }>()
+
+const longPressTimer = ref<NodeJS.Timeout>()
+const el = ref<HTMLElement | null>(null)
+
+const handleLongPress = () => {
+  console.log('long press')
+
+  el.value?.classList.remove('pressed')
+  el.value?.classList.toggle('selected')
+}
+const isSelected = ref(false)
+
+const touchHandler = {
+  touchstart: (event: TouchEvent) => {
+    el.value?.classList.add('pressed')
+    longPressTimer.value = setTimeout(() => {
+      handleLongPress()
+    }, 500)
+  },
+  touchmove: (event: TouchEvent) => {
+    clearTimeout(longPressTimer.value)
+    el.value?.classList.remove('pressed')
+  },
+  touchend: (event: TouchEvent) => {
+    clearTimeout(longPressTimer.value)
+    el.value?.classList.remove('pressed')
+  },
+}
 </script>
 
 <template>
-  <div ml4 mb2 flex items-center gap3>
+  <div ref="el" px3 py1 mx1 mb2px flex items-center gap3 relative rounded-lg overflow-hidden v-on="touchHandler ">
+    <div class="press-marker" />
     <div
       rounded-full
       bg-base
@@ -33,4 +62,25 @@ defineProps<{
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.press-marker {
+  width: 30%;
+  height: 30%;
+  position: absolute;
+  background: #FFFFFF00;
+  left: 0;
+  right: 0;
+  margin: auto;
+  transition: all 0.25s;
+  transition-delay: 0.1s;
+}
+.selected {
+  background: #14b8a534;
+}
+.pressed > .press-marker {
+  width: 100%;
+  height: 100%;
+  background: #ffffff10;
+
+}
+</style>
