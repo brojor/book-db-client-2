71cdfe63d6911182ecf8874e96f224d1d283c09e
brojor <bronislav.jordan@gmail.com>
2022-09-21 10:08:07 +0200

    fix: do not redirect to enterPage after page reload if user is remembered

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -10,7 +10,7 @@ interface Credentials {
 export const useUserStore = defineStore({
   id: 'user',
   state: () => ({
-    token: '',
+    token: localStorage.getItem('token') || '',
   }),
   getters: {
     isAuthenticated: state => !!state.token,
