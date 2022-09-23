15421b5cdb3ddfa7d6d9c623d2a8c1f8c8b51a6a
brojor <bronislav.jordan@gmail.com>
2022-09-23 13:38:10 +0200

    feat: add icons to TabsHeader

--- src/components/TabItem.vue
+++ src/components/TabItem.vue
@@ -2,14 +2,17 @@
 defineProps<{
   title: string
   isActive: boolean
+  icon: string
 }>()
 </script>
 
 <template>
-  <div
-    px2 rounded m1
-    :class="{ 'bg-accent text-white': isActive }"
-  >
-    {{ title }}
+  <div flex rounded m1 :class="{ 'bg-accent text-white': isActive }" px1>
+    <div flex items-center pl-2>
+      <div :class="icon" />
+    </div>
+    <div v-if="isActive" p2 text-center leading-5>
+      {{ title }}
+    </div>
   </div>
 </template>
--- src/components/TabsHeader.vue
+++ src/components/TabsHeader.vue
@@ -7,15 +7,25 @@ defineProps<{
   collections: CollectionList
 }>()
 const collectionStore = useCollectionStore()
+
+const icons = [
+  'i-mdi:home',
+  'i-mdi:gift-outline',
+  'i-mdi:checkbox-marked-circle-outline',
+  'i-mdi:progress-clock',
+  'i-mdi:close-circle-outline',
+] as const
 </script>
 
 <template>
-  <div name="tabs" flex bg-canvas px4 pb2>
+  <div name="tabs" flex bg-canvas px4>
     <TabItem
       v-for="(collection, index) in collections"
       :key="collection.id"
-      :title="collection.title"
+      :title="collectionList[index].title"
+      :index="index"
       :is-active="collection.id === collectionStore.activeCollectionName"
+      :icon="icons[index]"
       @click="swiperInstance?.slideTo(index)"
     />
   </div>
