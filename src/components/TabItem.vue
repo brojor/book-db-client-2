ecbc76a87ba0e6a2863723001fb7f2825211ad8e
brojor <bronislav.jordan@gmail.com>
2022-10-02 15:57:35 +0200

    feat(TabItem): animate width change

--- src/components/TabItem.vue
+++ src/components/TabItem.vue
@@ -9,13 +9,16 @@ defineProps<{
 
 <template>
   <div
-    flex rounded m1 px1 transition duration-500
+    flex rounded m1 transition-all duration-800 px1
     :style="{ backgroundColor: `hsla(var(--c-${name}), 0.1)` }" :class="{ '!bg-primary:40 text-white': isActive }"
   >
-    <div flex items-center p-2>
+    <div flex items-center px2>
       <div :class="icon" />
     </div>
-    <div v-if="isActive" py3 pr2 text-center leading-5>
+    <div
+      py2 max-w-0 transition-all duration-300 overflow-hidden whitespace-nowrap
+      :class="{ '!max-w-300 pr2': isActive }"
+    >
       {{ title }}
     </div>
   </div>
