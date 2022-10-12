579a83f29a459b5563358b43085c7158f7aa9a30
brojor <bronislav.jordan@gmail.com>
2022-10-12 18:22:35 +0200

    fix(AddBookIcon): Text below the button is visible for transparency

--- src/components/AddBookButton.vue
+++ src/components/AddBookButton.vue
@@ -6,10 +6,10 @@ const collection = computed(() =>
 </script>
 
 <template>
-  <RouterLink
-    to="/add-book" flex items-center justify-center absolute bottom-16 right-6 z-2 h-14 w-14 text-white rounded-full
-    dark:bg-primary:40 bg-primary:80
-  >
-    <div :class="collection !== 'wishlist' ? 'i-bx:book-add' : 'i-bx:book-heart'" text-2xl />
-  </RouterLink>
+  <div bg-canvas absolute bottom-16 right-6 z-2 rounded-full overflow-hidden>
+    <RouterLink to="/add-book" text-white dark:bg-primary:40 bg-primary:80 rounded-full h-14 w-14 flex-center>
+      <div :class="collection !== 'wishlist' ? 'i-bx:book-add' : 'i-bx:book-heart'" text-2xl />
+    </RouterLink>
+  </div>
+
 </template>
