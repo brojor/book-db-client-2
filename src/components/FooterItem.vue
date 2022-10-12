f1c7213d368f75737473be6a4bbc86e1e6358b63
brojor <bronislav.jordan@gmail.com>
2022-10-12 15:26:49 +0200

    fix(MainFooter): author icon)

--- src/components/FooterItem.vue
+++ src/components/FooterItem.vue
@@ -9,12 +9,10 @@ defineProps<{
 
 <template>
   <button flex items-center p2 :class="{ 'text-primary': isActive }">
-    <div i-ph:book-open text-xl />
+    <div :class="icon" text-xl />
     <span px2>{{ title }}</span>
-    <span
-      px2 text-high-emphasis rounded-lg font-normal
-      :class="isActive ? 'dark:bg-primary:40 bg-primary:80 text-white' : 'bg-gray:20 text-medium-emphasis' "
-    >
+    <span px2 text-high-emphasis rounded-lg font-normal
+      :class="isActive ? 'dark:bg-primary:40 bg-primary:80 text-white' : 'bg-gray:20 text-medium-emphasis' ">
       {{ count }}
     </span>
   </button>
