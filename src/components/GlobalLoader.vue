81148a7fd4047c483da219509a80d1ee5efdc63f
brojor <bronislav.jordan@gmail.com>
2022-10-15 13:53:52 +0200

    feat(GlobalLoader): Add transition

--- src/components/GlobalLoader.vue
+++ src/components/GlobalLoader.vue
@@ -1,11 +1,12 @@
-<template>
-    <div v-if="isLoading" absolute w-full h-full bg-black:20 backdrop-blur-2px z-100 flex-center>
-        <div i-mdi:loading text-8xl animate-spin></div>
-    </div>
-</template>
-
-
 <script setup lang="ts">
 const { isLoading, enableLoader } = useGlobalLoader()
 enableLoader()
 </script>
+
+<template>
+  <Transition name="fade">
+    <div v-if="isLoading" absolute w-full h-full bg-black:20 backdrop-blur-2px z-100 flex-center>
+      <div i-mdi:loading text-8xl animate-spin />
+    </div>
+  </Transition>
+</template>
